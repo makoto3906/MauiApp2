c14b9e0a432bc1bfbf72c6534c25db0e0908f6a6
makoto3906 <maum@nifty.com>
2023-08-06 21:58:54 +0900

    CollectionView Binding Monkeys

--- MauiApp2/MainPage.xaml.cs
+++ MauiApp2/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiApp2;
+﻿using MauiApp2.ViewModels;
+
+namespace MauiApp2;
 
 public partial class MainPage : ContentPage
 {
@@ -7,18 +9,20 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
-	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+        BindingContext = new MonkeysViewModel();
+    }
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+	//private void OnCounterClicked(object sender, EventArgs e)
+	//{
+	//	count++;
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+	//	if (count == 1)
+	//		CounterBtn.Text = $"Clicked {count} time";
+	//	else
+	//		CounterBtn.Text = $"Clicked {count} times";
+
+	//	SemanticScreenReader.Announce(CounterBtn.Text);
+	//}
 }
 
